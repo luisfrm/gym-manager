71af502b34d55b123a20b01a8ea88335a1167913
Luis Rivas <luisfrm_@outlook.com>
2025-01-01 19:33:15 -0400

    added new fonts Poppins as sans serif and Playfair Display as serif

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -53,7 +53,11 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		}
+  		},
+			fontFamily: {
+				sans: ['Poppins', 'sans-serif'],
+				serif: ['Playfair Display', 'serif'],
+			}
   	}
   },
   plugins: [require("tailwindcss-animate")],
