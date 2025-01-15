3823dd66649fccc781af9987e2bf9d6264d952e3
Luis Rivas <luisfrm_@outlook.com>
2025-01-15 03:22:01 -0400

    implemented new api methods for getting clients and validate token

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,11 @@
 import { API_URL } from "@/lib/config";
-import { LoginRequest, LoginResponse } from "@/lib/types";
+import { Client, LoginRequest, LoginResponse, ValidateTokenResponse } from "@/lib/types";
 import axios from "axios";
+
+const getToken = () => {
+  const token = localStorage.getItem("token");
+  return token ? JSON.parse(token) : null;
+};
 
 const api = axios.create({
   baseURL: API_URL,
@@ -9,7 +14,23 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) config.headers["Authorization"] = `Bearer ${token}`;
+  return config;
+});
+
 export const loginRequest = async (req: LoginRequest): Promise<LoginResponse> => {
   const res = await api.post("/auth/login", req);
+  return res.data;
+};
+
+export const getClientsRequest = async (): Promise<Client[]> => {
+  const res = await api.get("/clients");
+  return res.data;
+};
+
+export const validateTokenRequest = async (): Promise<ValidateTokenResponse> => {
+  const res = await api.get("/auth/validate");
   return res.data;
 };
