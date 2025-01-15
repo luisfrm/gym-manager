f92bbcb43d3013d00f7c4bf68bc41d4a83d2cca7
Luis Rivas <luisfrm_@outlook.com>
2025-01-15 03:22:57 -0400

    implemented form group for grouping inputs, label in a form

--- frontend/src/components/FormGroup.tsx
+++ frontend/src/components/FormGroup.tsx
@@ -13,6 +13,6 @@ export const FormLabelError = ({ children, className }: Props) => {
   return <span className={cn("text-red-500 text-sm font-medium leading-none", className)}>{children}</span>;
 };
 
-export const FormGroup = ({ children, className }: Props) => {
-  return <div className={cn("flex flex-col gap-3", className)}>{children}</div>;
+export const FormGroup = ({ children, className="flex flex-col gap-2" }: Props) => {
+  return <div className={cn(className)}>{children}</div>;
 };
