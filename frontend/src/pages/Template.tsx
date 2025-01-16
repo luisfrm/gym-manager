8f8cc4fd5138d25256160c2f9f04324361288abe
Luis Rivas <luisfrm_@outlook.com>
2025-01-16 08:59:30 -0400

    updated padding on template

--- frontend/src/pages/Template.tsx
+++ frontend/src/pages/Template.tsx
@@ -7,7 +7,7 @@ const Template = ({ children, className }: { children: React.ReactNode, classNam
     <SidebarProvider>
       <Navigation />
 
-      <main className={cn("px-4 py-4 md:px-6 pt-20 lg:pt-4 flex-1", className)}>
+      <main className={cn("px-4 py-4 md:px-6 pt-20 lg:pt-4 flex-1 flex flex-col gap-4", className)}>
         {children}
       </main>
     </SidebarProvider>
