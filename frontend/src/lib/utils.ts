66f4b5a7dd371b317834ffe4ca06a58adccc75a0
Luis Rivas <luisfrm_@outlook.com>
2025-01-16 00:53:47 -0400

    added methods to utils on frontend

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const isEmailValid = (email = "") => {
+  return email !== "" ? emailRegex.test(email) : true;
+};
+
+export const isDateActive = (expirationDateStr: string | undefined) => {
+  if (!expirationDateStr) return false;
+  const expirationDate = new Date(expirationDateStr);
+  const currentDate = new Date();
+  return expirationDate > currentDate;
+};
+
+export const formatDate = (date?: string) => {
+  if (!date) return "";
+  const dateObj = new Date(date);
+  return dateObj.toLocaleDateString("es-ES", { day: "2-digit", month: "2-digit", year: "numeric" });
+};
