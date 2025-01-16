c1d8352e85a9724f8d9eb1206558fc7319c2d9eb
Luis Rivas <luisfrm_@outlook.com>
2025-01-16 00:54:29 -0400

    removed console.log unnecesary on useLocalStorage

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -13,7 +13,6 @@ const useLocalStorage = (key: string, initialValue: any) => {
   const setLocalStorage = (value: any) => {
     window.localStorage.setItem(key, JSON.stringify(value));
     setValue(value);
-    console.log("value", value)
   };
 
   return [value, setLocalStorage];
