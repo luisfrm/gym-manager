2274897b849a284278647b2d910ee4a195100ce1
Luis Rivas <luisfrm_@outlook.com>
2025-02-04 12:33:24 -0400

    added name to register

--- backend/src/schemas/auth.schema.ts
+++ backend/src/schemas/auth.schema.ts
@@ -1,6 +1,13 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required.",
+      invalid_type_error: "Name must be a string.",
+    })
+    .min(3, { message: "Name must be at least 3 characters long." })
+    .max(20, { message: "Name must be at most 20 characters long." }),
   username: z
     .string({
       required_error: "Username is required.",
